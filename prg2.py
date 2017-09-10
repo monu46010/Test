7c018514c6f7e3aada2bf6e9ca937f1bb1610d55
monu46010 <bharat@pusan.ac.kr>
2017-09-10 15:10:58 +0900

    add result comment to prg2

--- prg2.py
+++ prg2.py
@@ -16,6 +16,8 @@ n2 = input ("Enter the Second No.")
 n1=int(n1)
 n2=int(n2)
 
+#result
+
 print ("The sum is",sum_numbers(n1,n2))
 print ("The product is",multiply(n1,n2))
 print ("The difference is",substract(n1,n2))
