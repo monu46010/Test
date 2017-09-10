59e7f347ac12b9540b7de2ec39d60e5422e68cc0
monu46010 <bharat@pusan.ac.kr>
2017-09-10 14:32:06 +0900

    Added a product function to prg2

--- prg2.py
+++ prg2.py
@@ -1,8 +1,12 @@
 def sum_numbers(a,b):
 	return int(a)+int(b)
+
+def multiply(a,b):
+	return int(a)*int(b)
 	
 n1 = input ("Enter First No.")
 n2 = input ("Enter the Second No.")
 
-print (sum_numbers(n1,n2))
+print ("sum is",sum_numbers(n1,n2))
+print ("product is",multiply(n1,n2))
 #print ("The sum of {} and {} is {}".format(n1,n2,sum_numbers(n1,n2)))
