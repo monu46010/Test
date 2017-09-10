93f7a044f2fd40ebaec2513e819ded2343c126f2
monu46010 <bharat@pusan.ac.kr>
2017-09-10 15:24:03 +0900

    Added division to prg1

--- prg1.py
+++ prg1.py
@@ -2,9 +2,18 @@
 
 def sum_numbers(a,b):
 	return int(a)+int(b)
+
+def division(a,b):
+	if a>b:
+		return a/b
+	else:
+		return b/a
 	
 n1 = input ("Enter First No.")
 n2 = input ("Enter the Second No.")
+n1 = int(n1)
+n2 = int(n2)
 
 print (sum_numbers(n1,n2))
+print ("Division result",division(n1,n2))
 print ("The sum of {} and {} is {}".format(n1,n2,sum_numbers(n1,n2)))
