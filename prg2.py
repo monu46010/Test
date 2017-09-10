aa175053bd9357858a494076078b6cb1bee9fb20
monu46010 <bharat@pusan.ac.kr>
2017-09-10 14:57:37 +0900

    Added difference function to prg2

--- prg2.py
+++ prg2.py
@@ -4,9 +4,19 @@ def sum_numbers(a,b):
 def multiply(a,b):
 	return int(a)*int(b)
 	
+def substract(a,b):
+	if int(a)>int(b):
+		return a-b
+	else:
+		return b-a
+	
 n1 = input ("Enter First No.")
 n2 = input ("Enter the Second No.")
 
+n1=int(n1)
+n2=int(n2)
+
 print ("sum is",sum_numbers(n1,n2))
 print ("product is",multiply(n1,n2))
+print ("Difference is",substract(n1,n2))
 #print ("The sum of {} and {} is {}".format(n1,n2,sum_numbers(n1,n2)))
